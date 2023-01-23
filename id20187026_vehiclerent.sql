43df942c6d62d3d60df9504a5eabc36c9adf7d2d
Vivek Kumar Saurabh <101538942+vivekkumarsaurabh@users.noreply.github.com>
2023-01-23 10:45:15 +0530

    Add files via upload

--- id20187026_vehiclerent.sql
+++ id20187026_vehiclerent.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 22, 2023 at 06:30 PM
+-- Generation Time: Jan 23, 2023 at 05:06 AM
 -- Server version: 10.5.16-MariaDB
 -- PHP Version: 7.3.32
 
@@ -42,7 +42,8 @@ CREATE TABLE `agency` (
 --
 
 INSERT INTO `agency` (`id`, `name`, `email`, `category`, `pass`, `datetime`) VALUES
-(1, 'sankalp car rental', 'marinefive1@gmail.com', 'agency', '$2y$10$zkdytFIt5AH0fS.aHEY11eUidIF2umuseUXwZkgDuypTUNIfyJX9W', '2023-01-22 16:44:35');
+(1, 'sankalp car rental', 'marinefive1@gmail.com', 'agency', '$2y$10$zkdytFIt5AH0fS.aHEY11eUidIF2umuseUXwZkgDuypTUNIfyJX9W', '2023-01-22 16:44:35'),
+(2, 'Vivek Kumar Saurabh', 'vk@gmail.com', 'agency', '$2y$10$39Wz3i3Nxym67Z1Zeq9ANOzjbbeFV3h43M8ejfnGdXADnWoG0U99m', '2023-01-22 19:14:14');
 
 -- --------------------------------------------------------
 
@@ -59,6 +60,7 @@ CREATE TABLE `order_car` (
   `car_model` varchar(255) NOT NULL,
   `car_number` varchar(255) NOT NULL,
   `price` varchar(255) NOT NULL,
+  `email_owner` varchar(255) NOT NULL,
   `res` int(255) NOT NULL,
   `current_date_time` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -67,10 +69,9 @@ CREATE TABLE `order_car` (
 -- Dumping data for table `order_car`
 --
 
-INSERT INTO `order_car` (`id`, `c_name`, `c_email`, `days`, `date`, `car_model`, `car_number`, `price`, `res`, `current_date_time`) VALUES
-(1, 'Sankalp Singh', 'sankalp.singh117@gmail.com', '1', '2023-01-25', 'Mercedes S Class', 'HR 26DQ 5555 ', '128000', 0, '2023-01-22 17:32:07'),
-(2, 'Sankalp Singh', 'sankalp.singh117@gmail.com', '2', '2023-01-26', 'Mercedes S Class', 'HR 26DQ 5555 ', '256000', 0, '2023-01-22 18:03:19'),
-(3, 'Sankalp Singh', 'sankalp.singh117@gmail.com', '4', '2023-01-26', 'Mercedes S Class', 'HR 26DQ 5555 ', '16000', 0, '2023-01-22 18:10:13');
+INSERT INTO `order_car` (`id`, `c_name`, `c_email`, `days`, `date`, `car_model`, `car_number`, `price`, `email_owner`, `res`, `current_date_time`) VALUES
+(2, 'Vicky Kumar', 'vicky@gmail.com', '2', '2023-01-25', 'Alto 800', 'HR 26DQ 5551', '1600', 'marinefive1@gmail.com', 1, '2023-01-22 20:01:41'),
+(3, 'Sankalp Singh', 'sankalp.singh117@gmail.com', '1', '2023-01-28', 'Mercedes S Class', 'UP 26DQ 5590', '2000', 'marinefive1@gmail.com', 0, '2023-01-22 20:03:42');
 
 -- --------------------------------------------------------
 
@@ -93,7 +94,8 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `email`, `category`, `pass`, `datetime`) VALUES
 (1, 'Sankalp Singh', 'marinefive1@gmail.com', 'individual', '$2y$10$IrVQ1AhaI9FrvOUQSdJ0reoRnu9W58axoKm5IBs2V/Atq3q0Vkuhy', '2023-01-22 16:56:46'),
-(2, 'Sankalp Singh', 'sankalp.singh117@gmail.com', 'individual', '$2y$10$omr5yvh7tHrHqxUZqIcSCOtq7PD5KshfteZJJ.rMHcgie/BvqkQ3q', '2023-01-22 16:58:14');
+(2, 'Sankalp Singh', 'sankalp.singh117@gmail.com', 'individual', '$2y$10$omr5yvh7tHrHqxUZqIcSCOtq7PD5KshfteZJJ.rMHcgie/BvqkQ3q', '2023-01-22 16:58:14'),
+(3, 'Vicky Kumar', 'vicky@gmail.com', 'individual', '$2y$10$JTdLaW6wF5BjGWcHh0ii.OE3Tb0a8hEWxziKK4X5pUsahLlcyyRDy', '2023-01-22 19:46:25');
 
 -- --------------------------------------------------------
 
@@ -106,17 +108,18 @@ CREATE TABLE `vehicles` (
   `model` varchar(255) NOT NULL,
   `vnumber` varchar(255) NOT NULL,
   `seat` varchar(255) NOT NULL,
-  `rent` varchar(255) NOT NULL
+  `rent` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `vehicles`
 --
 
-INSERT INTO `vehicles` (`id`, `model`, `vnumber`, `seat`, `rent`) VALUES
-(1, 'Mercedes S Class', 'HR 26DQ 5555 ', '5', '4000'),
-(2, 'Alto 900', 'UP 26DQ 5590', '5', '1000'),
-(3, 'Suzuki Glanza', 'HR 26DQ 5545', '7', '1500');
+INSERT INTO `vehicles` (`id`, `model`, `vnumber`, `seat`, `rent`, `email`) VALUES
+(5, 'Toyota Fortuner', 'HR 26DQ 5555 ', '7', '2000', 'vk@gmail.com'),
+(6, 'Alto 800', 'HR 26DQ 5551', '5', '800', 'marinefive1@gmail.com'),
+(7, 'Mercedes S Class', 'UP 26DQ 5590', '5', '2000', 'marinefive1@gmail.com');
 
 --
 -- Indexes for dumped tables
@@ -154,7 +157,7 @@ ALTER TABLE `vehicles`
 -- AUTO_INCREMENT for table `agency`
 --
 ALTER TABLE `agency`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `order_car`
@@ -166,13 +169,13 @@ ALTER TABLE `order_car`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `vehicles`
 --
 ALTER TABLE `vehicles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
